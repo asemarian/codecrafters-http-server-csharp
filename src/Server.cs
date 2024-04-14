c65953a098224ffe8fa6bb989866391aa99baab5
Asem Zletni <asem.zletni@gmail.com>
2024-04-14 08:53:57 +0000

    return 404 for unsupported routes

--- src/Server.cs
+++ src/Server.cs
@@ -1,30 +1,68 @@
 using System.Net;
 using System.Net.Sockets;
+using System.Text.RegularExpressions;
 
-// You can use print statements as follows for debugging, they'll be visible when running tests.
-Console.WriteLine("Logs from your program will appear here!");
-
-try
+internal class Program
 {
-    // bind to a tcp port
-    TcpListener server = new TcpListener(IPAddress.Any, 4221);
-    server.Start();
+    const string OK = "HTTP/1.1 200 OK\r\n\r\n";
+    const string NotFound = "HTTP/1.1 404 Not Found\r\n\r\n";
 
-    // prepare response message
-    string response = "HTTP/1.1 200 OK\r\n\r\n";
-
-    // listen for incoming connection requests
-    while (true)
+    private static void Main(string[] args)
     {
-        using var client = server.AcceptTcpClient();
+        try
+        {
+            // bind to a tcp port
+            TcpListener server = new TcpListener(IPAddress.Any, 4221);
+            server.Start();
 
-        Console.WriteLine("Client accepted!");
+            // listen for incoming connection requests
+            while (true)
+            {
+                using var client = server.AcceptTcpClient();
 
-        var stream = client.GetStream();
-        byte[] byteReponse = System.Text.Encoding.ASCII.GetBytes(response);
-        stream.Write(byteReponse);
+                Console.WriteLine("Client accepted!");
+
+                var stream = client.GetStream();
+                byte[] buffer = new byte[256];
+
+                stream.Read(buffer, 0, buffer.Length);
+
+                var request = System.Text.Encoding.ASCII.GetString(buffer);
+
+                string? path = GetPath(request);
+
+                RespondToClient(path, stream);
+            }
+        }
+        catch (SocketException e)
+        {
+            Console.WriteLine($"SocketException: {e}");
+        }
+
     }
-} catch (SocketException e)
-{
-    Console.WriteLine($"SocketException: {e}");
+    private static string GetPath(string input)
+    {
+        string pattern = @"^\w+\s+([^\s]+)\s+HTTP/.*$";
+        Match match = Regex.Match(input, pattern, RegexOptions.Multiline);
+
+        if (match.Success)
+        {
+            return match.Groups[1].Value;
+        }
+
+        throw new ArgumentException("HTTP request is not valid");
+    }
+
+    private static void RespondToClient(string path, NetworkStream stream)
+    {
+        switch (path)
+        {
+            case "/":
+                stream.Write(System.Text.Encoding.ASCII.GetBytes(OK));
+                break;
+            default:
+                stream.Write(System.Text.Encoding.ASCII.GetBytes(NotFound));
+                break;
+        }
+    }
 }
