19f4818b5ffb80f7cff6edf988903addf3aa4c2c
Asem Zletni <asem.zletni@gmail.com>
2024-04-14 07:31:13 +0000

    send static response to incoming tcp connections

--- src/Server.cs
+++ src/Server.cs
@@ -4,7 +4,27 @@ using System.Net.Sockets;
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 Console.WriteLine("Logs from your program will appear here!");
 
-// Uncomment this block to pass the first stage
-TcpListener server = new TcpListener(IPAddress.Any, 4221);
-server.Start();
-server.AcceptSocket(); // wait for client
+try
+{
+    // bind to a tcp port
+    TcpListener server = new TcpListener(IPAddress.Any, 4221);
+    server.Start();
+
+    // prepare response message
+    string response = "HTTP/1.1 200 OK\r\n\r\n";
+
+    // listen for incoming connection requests
+    while (true)
+    {
+        using var client = server.AcceptTcpClient();
+
+        Console.WriteLine("Client accepted!");
+
+        var stream = client.GetStream();
+        byte[] byteReponse = System.Text.Encoding.ASCII.GetBytes(response);
+        stream.Write(byteReponse);
+    }
+} catch (SocketException e)
+{
+    Console.WriteLine($"SocketException: {e}");
+}
