6c8f39347592a1697c300b967f324145a966fd36
Asem Zletni <asem.zletni@gmail.com>
2024-04-15 10:24:17 +0000

    add http response builder custom class

---
+++ src/HttpResponseBuilder.cs
@@ -0,0 +1,44 @@
+using System.Net;
+
+class HttpResponseBuilder
+{
+    private string Delimiter { get; set; } = "\r\n";
+
+    private string StartLine { get; set; } = "HTTP/1.1";
+
+    private string Headers { get; set; } = "";
+
+    private string Body { get; set; } = "";
+
+    public HttpResponseBuilder()
+    {
+        Reset();
+    }
+
+    public void Reset()
+    {
+        Headers = Body = string.Empty;
+    }
+
+    public void SetStatusCode(HttpStatusCode statusCode)
+    {
+        StartLine = $"{StartLine} {(int)statusCode} {statusCode}{Delimiter}";
+    }
+
+    public void SetHeader(string key, string value)
+    {
+        Headers += $"{key.Trim()}: {value.Trim()}{Delimiter}";
+    }
+
+    public void SetBody(string body)
+    {
+        Body = body;
+    }
+
+    public string GetResponse()
+    {
+        var result = $"{StartLine}{Headers}{Delimiter}{Body}";
+        Reset();
+        return result;
+    }
+}
